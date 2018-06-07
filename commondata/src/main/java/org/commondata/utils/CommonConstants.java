a05beb17c469dc4ce9eb9266ba5f10fe2881575b
twb <tianwenbin104@qq.com>
2018-06-07 12:11:18 +0800

    公共

--- commondata/src/main/java/org/commondata/utils/CommonConstants.java
+++ commondata/src/main/java/org/commondata/utils/CommonConstants.java
@@ -13,5 +13,23 @@ public class CommonConstants
 			.addSerializationExclusionStrategy(new MyExclusionStrategy())//设置序列化时字段采用策略
 			.serializeNulls().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
 			.create();
+	
+	
 
+	//提交上链的参数
+	public static final String COMMITCHAIN_PRODUCER_ID="PID_jingtumtwb";
+	public static final String COMMITCHAIN_TOPIC="jingtum";
+	public static final String COMMITCHAIN_TAG="commitchain";
+	public static final String COMMITCHAIN_CONSUMER_ID="CID_JingtumCommitchain";
+	public static final String COMMITCHAIN_ONSADDR="http://onsaddr-internet.aliyun.com/rocketmq/nsaddr4client-internet";
+	
+//			#ONSADDR 请根据不同Region进行配置
+//			#公网测试: http://onsaddr-internet.aliyun.com/rocketmq/nsaddr4client-internet
+//			#公有云生产: http://onsaddr-internal.aliyun.com:8080/rocketmq/nsaddr4client-internal
+//			#杭州金融云: http://jbponsaddr-internal.aliyun.com:8080/rocketmq/nsaddr4client-internal
+//			#深圳金融云: http://mq4finance-sz.addr.aliyun.com:8080/rocketmq/nsaddr4client-internal
+
+	
+	
+	
 }
--- commondata/src/main/java/org/commondata/utils/MQUtils.java
+++ commondata/src/main/java/org/commondata/utils/MQUtils.java
@@ -1,13 +1,18 @@
 package org.commondata.utils;
 
+import java.util.Properties;
+
 import org.commondata.data.CommitChainRespMqData;
 import org.commondata.data.CommitchainMqData;
 import org.commondata.data.DistributeMqData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.aliyun.openservices.ons.api.Consumer;
 import com.aliyun.openservices.ons.api.Message;
+import com.aliyun.openservices.ons.api.ONSFactory;
 import com.aliyun.openservices.ons.api.Producer;
+import com.aliyun.openservices.ons.api.PropertyKeyConst;
 import com.aliyun.openservices.ons.api.SendResult;
 
 public class MQUtils
@@ -64,6 +69,20 @@ public class MQUtils
 	}
 	/**
 	 * 
+	 * @Title: sendCommitChainMQ   
+	 * @Description: 发送上链的MQ
+	 * @param: @param producer
+	 * @param: @param data
+	 * @param: @return      
+	 * @return: SendResult      
+	 * @throws
+	 */
+	public static SendResult sendCommitChainMQ(Producer producer,CommitchainMqData data)
+	{
+		return sendMQ(producer, CommonConstants.COMMITCHAIN_TOPIC, CommonConstants.COMMITCHAIN_TAG, CommonConstants.PRETTY_PRINT_GSON.toJson(data));
+	}
+	/**
+	 * 
 	 * @Title: sendMQ   
 	 * @Description:发送MQ上链确认反馈数据
 	 * @param: @param producer
@@ -96,6 +115,15 @@ public class MQUtils
 		return sendMQ(producer, topic, tag, CommonConstants.PRETTY_PRINT_GSON.toJson(data));
 	}
 	
+	/**
+	 * 
+	 * @Title: getCommitchainMqData   
+	 * @Description: 解析上链的MQ数据
+	 * @param: @param data
+	 * @param: @return      
+	 * @return: CommitchainMqData      
+	 * @throws
+	 */
 	public static CommitchainMqData getCommitchainMqData(String data)
 	{
 		if(data==null||data.length()==0)
@@ -105,6 +133,15 @@ public class MQUtils
 		return CommonConstants.PRETTY_PRINT_GSON.fromJson(data, CommitchainMqData.class);
 	}
 	
+	/**
+	 * 
+	 * @Title: getCommitChainRespMqData   
+	 * @Description: 解析上链结果
+	 * @param: @param data
+	 * @param: @return      
+	 * @return: CommitChainRespMqData      
+	 * @throws
+	 */
 	public static CommitChainRespMqData getCommitChainRespMqData(String data)
 	{
 		if(data==null||data.length()==0)
@@ -113,7 +150,15 @@ public class MQUtils
 		}
 		return CommonConstants.PRETTY_PRINT_GSON.fromJson(data, CommitChainRespMqData.class);
 	}
-	
+	/**
+	 * 
+	 * @Title: getDistributeMqData   
+	 * @Description: 解析分发的数据
+	 * @param: @param data
+	 * @param: @return      
+	 * @return: DistributeMqData      
+	 * @throws
+	 */
 	public static DistributeMqData getDistributeMqData(String data)
 	{
 		if(data==null||data.length()==0)
@@ -122,7 +167,16 @@ public class MQUtils
 		}
 		return CommonConstants.PRETTY_PRINT_GSON.fromJson(data, DistributeMqData.class);
 	}
-	
+	/**
+	 * 
+	 * @Title: fromJson   
+	 * @Description: json转对象
+	 * @param: @param data
+	 * @param: @param cla
+	 * @param: @return      
+	 * @return: T      
+	 * @throws
+	 */
 	public static <T> T fromJson(String data,Class<T> cla)
 	{
 		if(data==null||data.length()==0)
@@ -131,10 +185,66 @@ public class MQUtils
 		}
 		return CommonConstants.PRETTY_PRINT_GSON.fromJson(data, cla);
 	}
-	
+	/**
+	 * 
+	 * @Title: toJson   
+	 * @Description: 对象转json
+	 * @param: @param obj
+	 * @param: @return      
+	 * @return: String      
+	 * @throws
+	 */
 	public static String toJson(Object obj)
 	{
 		return CommonConstants.PRETTY_PRINT_GSON.toJson(obj);
 	}
+	
+	/**
+	 * 
+	 * @Title: createConsumer   
+	 * @Description: 创建消费者
+	 * @param: @param consumerProperties
+	 * @param: @return      
+	 * @return: Consumer      
+	 * @throws
+	 */
+	public static Consumer createConsumer(Properties consumerProperties)
+	{
+		String onsAddr = (String) consumerProperties.get(PropertyKeyConst.ONSAddr);
+		if(onsAddr==null||onsAddr.isEmpty())
+		{
+			consumerProperties.setProperty(PropertyKeyConst.ONSAddr, CommonConstants.COMMITCHAIN_ONSADDR);
+		}
+		
+		return ONSFactory.createConsumer(consumerProperties);
+	}
+	
+	/**
+	 * 
+	 * @Title: createProducer   
+	 * @Description: 创建生产者
+	 * @param: @param producerProperties
+	 * @param: @return      
+	 * @return: Producer      
+	 * @throws
+	 */
+	public static Producer createProducer(Properties producerProperties)
+	{
+		String onsAddr = (String) producerProperties.get(PropertyKeyConst.ONSAddr);
+		if(onsAddr==null||onsAddr.isEmpty())
+		{
+			producerProperties.setProperty(PropertyKeyConst.ONSAddr, CommonConstants.COMMITCHAIN_ONSADDR);
+		}
+		String producer_id = (String) producerProperties.get(PropertyKeyConst.ProducerId);
+		if(producer_id==null||producer_id.isEmpty())
+		{
+			producerProperties.setProperty(PropertyKeyConst.ProducerId, CommonConstants.COMMITCHAIN_PRODUCER_ID);
+		}
+		return ONSFactory.createProducer(producerProperties);
+	}
+	
+	
+	
+	
 	
 }
